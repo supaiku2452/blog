54ea20615a63cb9f97fe172bd4ed4045b2d5da3f
supaiku2452 <supaiku2452@gmail.com>
2020-06-29 00:29:38 +0900

    Fix a blog-post layout.

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,82 +1,9 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
+import { graphql } from "gatsby"
+import { Layout } from "./layout"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import { rhythm, scale } from "../utils/typography"
-import { Tags } from "../components/tags"
-
-const BlogPostTemplate = ({ data, pageContext, location }) => {
-  const post = data.markdownRemark
-  const siteTitle = data.site.siteMetadata.title
-  const { previous, next } = pageContext
-  const tags = post.frontmatter.tags ?? []
-
-  return (
-    <Layout location={location} title={siteTitle}>
-      <SEO
-        title={post.frontmatter.title}
-        description={post.frontmatter.description || post.excerpt}
-      />
-      <article>
-        <header>
-          <h1
-            style={{
-              marginTop: rhythm(1),
-              marginBottom: 0,
-            }}
-          >
-            {post.frontmatter.title}
-          </h1>
-          <p
-            style={{
-              ...scale(-1 / 5),
-              display: `block`,
-            }}
-          >
-            {post.frontmatter.date}
-          </p>
-          <p>
-            <Tags tags={tags} />
-          </p>
-        </header>
-        <section dangerouslySetInnerHTML={{ __html: post.html }} />
-        <hr
-          style={{
-            marginBottom: rhythm(1),
-          }}
-        />
-        <footer />
-      </article>
-
-      <nav>
-        <ul
-          style={{
-            display: `flex`,
-            flexWrap: `wrap`,
-            justifyContent: `space-between`,
-            listStyle: `none`,
-            padding: 0,
-          }}
-        >
-          <li>
-            {previous && (
-              <Link to={previous.fields.slug} rel="prev">
-                ← {previous.frontmatter.title}
-              </Link>
-            )}
-          </li>
-          <li>
-            {next && (
-              <Link to={next.fields.slug} rel="next">
-                {next.frontmatter.title} →
-              </Link>
-            )}
-          </li>
-        </ul>
-      </nav>
-    </Layout>
-  )
+const BlogPostTemplate = ({ data, pageContext }) => {
+  return <Layout data={data} pageContext={pageContext} />
 }
 
 export default BlogPostTemplate
---
+++ src/templates/layout.js
@@ -0,0 +1,88 @@
+import React from "react"
+import { Header } from "../components/header"
+import SEO from "../components/seo"
+import { rhythm } from "../utils/typography.js"
+import { Link } from "gatsby"
+import { Tags } from "../components/tags"
+import styled from "styled-components"
+import { PublishDate } from "../components/publish-date"
+import { Footer } from "../components/footer"
+
+export const Layout = ({ data, pageContext }) => {
+  const post = data.markdownRemark
+  const { previous, next } = pageContext
+  const tags = post.frontmatter.tags ?? []
+  return (
+    <div>
+      <Header />
+      <SEO
+        title={post.frontmatter.title}
+        description={post.frontmatter.description || post.excerpt}
+      />
+      <Article>
+        <ArticleHeader>
+          <H1>{post.frontmatter.title}</H1>
+          <InfoHeader>
+            <PublishDate date={post.frontmatter.date} />
+            {tags.length > 0 && <Tags tags={tags} />}
+          </InfoHeader>
+        </ArticleHeader>
+        <section dangerouslySetInnerHTML={{ __html: post.html }} />
+        <HR />
+        <footer />
+      </Article>
+
+      <Nav>
+        <UL>
+          <li>
+            {previous && (
+              <Link to={previous.fields.slug} rel="prev">
+                ← {previous.frontmatter.title}
+              </Link>
+            )}
+          </li>
+          <li>
+            {next && (
+              <Link to={next.fields.slug} rel="next">
+                {next.frontmatter.title} →
+              </Link>
+            )}
+          </li>
+        </UL>
+      </Nav>
+      <Footer />
+    </div>
+  )
+}
+
+const Article = styled.article`
+  padding: 0 30px;
+`
+const ArticleHeader = styled.header`
+  margin-bottom: 16px;
+`
+
+const H1 = styled.h1`
+  margin-top: ${rhythm(1)};
+  margin-bottom: 0;
+`
+const InfoHeader = styled.div`
+  display: flex;
+  > div:not(:first-child) {
+    margin-left: 8px;
+  }
+`
+const Nav = styled.nav`
+  padding: 0 30px;
+`
+const HR = styled.hr`
+  margin-bottom: ${rhythm(1)};
+`
+const UL = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  list-style: none;
+  padding: 0;
+  margin-left: 0;
+`
